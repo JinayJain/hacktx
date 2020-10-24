1e9fb5ff72302856f16aebd987920be37480ec00
Daniel <dm32@illinois.edu>
2020-10-24 12:47:45 -0500

    not tested members card

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Home from './pages/Home'
-
+import Result from './pages/Result'
 import { Switch, Redirect, Route, BrowserRouter as Router } from 'react-router-dom'
 
 function App () {
@@ -15,6 +15,9 @@ function App () {
           </Route>
           <Route path="/">
             <Home />
+          </Route>
+          <Route path="/members">
+            <Result/>
           </Route>
         </Switch>
       </div>
---
+++ client/src/components/Card/card.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect, setState} from 'react';
+import 'antd/dist/antd.css';
+import { Card, Button } from 'antd';
+import './styles.css';
+import { useHistory } from "react-router-dom";
+
+
+function Members() {
+    const  [members,setMembers]= useState([{"first":"Lamar","last":"Alexander","party":"R","state":"TN","hometown":"Maryville", "district": "5th"}]);
+    
+    let returnParty;
+    return (
+        <div className="member-container">
+            {members && members.map((data, key) => {
+                if ((data.party) == "R"){
+                    returnParty="Republican";
+                } else if (data.party == "D") {
+                    returnParty="Democrat";
+                } else {
+                    returnParty="Independent";
+                }
+                return (
+                    <div key={key}>
+                        <Member
+                            name={data.first + " " + data.last}
+                            state={data.state}
+                            district={data.district}
+                            hometown={data.hometown}
+                            party= {returnParty}
+                        />
+                    </div>
+                )
+            })}
+
+        </div>
+    );
+}
+
+const Member = ({name, image, state, district, hometown, party}) => {
+    let history = useHistory();
+    function toProfile(name) {
+        history.push("/member/"+name);
+    }
+    return (
+        <div className="member-card">
+            <img src={image}></img>
+            <div className="party">{party}</div>
+            <h2><strong>{name}</strong></h2>
+            <h4><strong>State: </strong>{state}</h4>
+            <h4><strong>District: </strong>{district}</h4>
+            <h4><strong>Hometown: </strong>{hometown}</h4>
+            <Button type="primary" shape="round" size='large' onClick={toProfile(name.trim())}>
+                Candidate Profile
+            </Button>
+        </div>
+    );
+};
+
+export default Members;
