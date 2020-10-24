d7c46c9be070af23b926e2e5af7a4a6a80f6fdda
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-10-24 09:43:44 -0700

    search bar

---
+++ client/src/components/Searchbar/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+const Searchbar = () => {
+  return (
+    <div>
+        Search Bar wow
+    </div>
+  )
+}
+
+export default Searchbar
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-//import Navbar from '@/components/Navbar'
+import Navbar from '../../components/Navbar'
 
 const Home = () => {
   return (
