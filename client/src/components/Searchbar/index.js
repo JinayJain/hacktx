213c261c673d4430a4d0cd623b286e5dae87cc57
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-10-24 10:29:26 -0700

    homepage no long says ok wow

--- client/src/components/Searchbar/index.js
+++ client/src/components/Searchbar/index.js
@@ -1,14 +1,25 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { Input } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import './styles.css'
+
+const searchKeywords = (keyword) => {
+  // find all members
+  let members;
+  fetch("http://localhost:5000/members")
+        .then(res => res.text())
+        .then(res => members);
+}
 
 const Searchbar = () => {
   return (
-    <div>
-        Search Bar wow
-        <Input size="large" placeholder="SEARCH BY KEYWORDS" suffix={<UserOutlined />} />
-        <br />
-    </div>
+    <Input.Search 
+      className="keywords"
+      size="large"
+      placeholder="SEARCH BY KEYWORDS" 
+      allowClear 
+      style={{ width: '70%' }} 
+      onChange={(e) => searchKeywords(e.target.value)}
+    />
   )
 }
 
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,14 +1,20 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
 import Searchbar from '../../components/Searchbar'
+import './styles.css'
+import logo from '../../logo.svg'
+
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 const Home = () => {
   return (
-    <div>
-        <Navbar/>
-        <Searchbar/>
-      Ok
-    </div>
+    <Content>
+        <div className="cover">
+            <img src={logo}/>
+            <Searchbar/>
+        </div>
+    </Content>
   )
 }
 
