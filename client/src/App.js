6d4ac0ef80c04c5414825485877ae2619dcbe117
Kincent <kinlan19@bergen.org>
2020-10-24 16:15:11 -0400

    oops accidently deleted App.js

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Home from './pages/Home'
+
+import { Switch, Redirect, Route, BrowserRouter as Router } from 'react-router-dom'
+
+function App () {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/test">
+            <div>yo yo yo o</div>
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
