3beb2da12251c1fb0a80f9a99d6fbd1f68b1c422
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-10-24 11:29:12 -0700

    search feature functional for senate :D can search by party, location, f/l name etc

--- client/src/components/Searchbar/index.js
+++ client/src/components/Searchbar/index.js
@@ -2,25 +2,96 @@ import React, { useState, useContext, useEffect } from 'react'
 import { Input } from 'antd';
 import './styles.css'
 
-const searchKeywords = (keyword) => {
-  // find all members
-  let senate = fetch("http://localhost:3000/api/senate/members")
-        .then(res => res.json());
-  console.log(senate);
-  senate.forEach(member => {
-    console.log(member);
-  });
+const searchKeywords = (keywords) => {
+  let result = [];
+  keywords = keywords.toLowerCase();
+
+
+  // find all matching in senate
+  let senate = fetch("http://localhost:8080/api/senate/members", {
+              headers:{
+                  "accepts":"application/json"
+              }
+        })
+        .then(res => res.json())
+        .then(data => {
+          switch(keywords) {
+            case("democrat"): 
+              data.forEach(member => {
+                // if keyword matches
+                if (member.party == "D") {
+                  console.log(member);
+                  result.push(member);
+                }
+              });
+              break;
+            case("republican"): 
+              data.forEach(member => {
+                // if keyword matches
+                if (member.party == "R") {
+                  console.log(member);
+                  result.push(member);
+                }
+              });
+              break;
+            case("independent"): 
+              data.forEach(member => {
+                // if keyword matches
+                if (member.party == "I") {
+                  console.log(member);
+                  result.push(member);
+                }
+              });
+              break;
+            default:
+              data.forEach(member => {
+                // if keyword matches
+                let member_string = JSON.stringify(member).toLowerCase();
+                if (member_string.includes(keywords)) {
+                  console.log(member);
+                  result.push(member);
+                }
+              });
+              break;
+          }
+        });
+  console.log(keywords);
 }
 
 const Searchbar = () => {
+
+  const initialSearchData = Object.freeze({
+    keywords: "",
+  });
+
+  const [formData, updateFormData] = React.useState(initialSearchData);
+
+  const handleChange = (event) => {
+    console.log("EVENT: "+ event);
+    updateFormData({
+        ...formData,
+        // Trimming any whitespace
+        keywords: document.getElementById("keywords").value.trim()
+      });
+  }
+
+  const handleKeyDown = (event) => {
+    console.log(event);
+    if (event.key === 'Enter') {
+      searchKeywords(formData.keywords);
+    }
+  }
+
   return (
     <Input.Search 
       className="keywords"
+      id="keywords"
       size="large"
       placeholder="SEARCH BY KEYWORDS" 
       allowClear 
       style={{ width: '70%' }} 
-      onChange={(e) => searchKeywords(e.target.value)}
+      onKeyDown={handleKeyDown}
+      onChange={(e) => handleChange(e.target.value)}
     />
   )
 }
