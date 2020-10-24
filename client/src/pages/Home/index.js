49e0677be64bcaba1274eecc6ab82aca84927b90
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-10-24 15:23:45 -0700

    map

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,11 +3,62 @@ import logo from '../../logo.svg'
 
 import './styles.css'
 import Searchbar from '../../components/Searchbar'
+import MapComponent from '../../components/Map/MapComponent'
 
 import { Layout } from 'antd';
 import { Button } from 'antd';
 import { Row, Col } from 'antd';
+
 const { Header, Footer, Sider, Content } = Layout;
+
+const Maps = () => {
+
+  const US_BOUNDS = {
+      north: 55,
+      south: 25,
+      east: -60,
+      west: -140
+  };   
+
+  const ALASKA_BOUNDS = {
+      north: 75,
+      south: 50,
+      east: -135,
+      west: -175
+  };   
+  
+  const HAWAII_BOUNDS = {
+      north: 25,
+      south: 15,
+      east: -150,
+      west: -165
+  }
+
+  const containerUSConnected = {
+      width: '60vw',
+      height: '80vh'
+  };
+
+  const containerAlaskaHawaii = {
+      width: '30vw',
+      height: '30vh'
+  };
+
+  return ( <>
+      <Row>
+          <Col span={16}>
+              <MapComponent containerStyle={containerUSConnected} defaultZoom={4} center={ {  lat: 40.78, lng: -101.76 }} borderBounds={US_BOUNDS}/>
+          </Col>
+          <Col span={8}>
+              <br/><br/><br/>
+              <MapComponent containerStyle={containerAlaskaHawaii} defaultZoom={5} center={ {  lat: 61.370716, lng: -152.404419 }} borders={ALASKA_BOUNDS}/>
+              <br/>
+              <MapComponent containerStyle={containerAlaskaHawaii} defaultZoom={6} center={ {  lat: 21.094318, lng: -157.498337 }} borders={HAWAII_BOUNDS}/>
+          </Col>
+      </Row>
+      </>
+  )
+}
 
 const Home = () => {
   const resultRef = React.useRef(null);
@@ -53,8 +104,8 @@ const Home = () => {
           <h1>WOW! INfoRmaTiON!</h1>
         </div>
         <div id="map_section" ref={mapRef}>
-          <h1>WOW! map!</h1>
-          
+          <h1>Filter by location </h1>
+          <Maps/>
         </div>
         <div id="result_section" ref={resultRef}>
           <h1>Search Result</h1>
