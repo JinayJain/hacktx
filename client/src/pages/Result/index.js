3190bac78fc32708fcf35388045683f448070d39
Daniel <dm32@illinois.edu>
2020-10-24 20:46:33 -0500

    added array passing

--- client/src/pages/Result/index.js
+++ client/src/pages/Result/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Members from '../../components/Card/card';
 const Result = () => {
     return(
-        <Members/>
+        <Members
+            array={[{"first":"Lamar","last":"Alexander","party":"R","state":"TN","hometown":"Maryville"}]}
+        />
     );
 }
 
